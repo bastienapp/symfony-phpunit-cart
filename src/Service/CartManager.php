7d34e6537aac76d15148e7920eff88dc0ed8ddc6
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-02-08 16:07:31 +0100

    init

--- src/Service/CartManager.php
+++ src/Service/CartManager.php
@@ -2,12 +2,27 @@
 
 namespace App\Service;
 
+use App\Entity\Cart;
 use App\Entity\CartItem;
 use App\Entity\Product;
+use App\Repository\CartItemRepository;
+use App\Repository\CartRepository;
+use App\Repository\ProductRepository;
 use InvalidArgumentException;
 
 class CartManager
 {
+    private ProductRepository $productRepository;
+    private CartRepository $cartRepository;
+    private CartItemRepository $cartItemRepository;
+
+    public function __construct(ProductRepository $productRepository, CartRepository $cartRepository, CartItemRepository $cartItemRepository)
+    {
+        $this->productRepository = $productRepository;
+        $this->cartRepository = $cartRepository;
+        $this->cartItemRepository = $cartItemRepository;
+    }
+
     public function getCartItemTotal(?CartItem $cartItem): float
     {
         if ($cartItem === null) {
@@ -16,5 +31,29 @@ class CartManager
         /** @var Product $product */
         $product = $cartItem->getProduct();
         return $cartItem->getQuantity() * $product->getUnitPrice();
+    }
+
+    public function addProductToCart(int $productId, int $cartId): CartItem
+    {
+        $product = $this->productRepository->find($productId);
+        if ($product === null) {
+            throw new InvalidArgumentException("Product $productId doesn't exist");
+        }
+        $cart = $this->cartRepository->find($cartId);
+        if ($cart === null) {
+            throw new InvalidArgumentException("Cart $cartId doesn't exist");
+        }
+
+        $cartItem = $this->cartItemRepository->findOneBy(['product' => $product, 'cart' => $cart]);
+        if ($cartItem === null) {
+            $cartItem = new CartItem();
+            $cartItem->setProduct($product);
+            $cartItem->setCart($cart);
+            $cartItem->setQuantity(1);
+        } else {
+            $cartItem->setQuantity($cartItem->getQuantity() + 1);
+        }
+
+        return $cartItem;
     }
 }
--- tests/Service/CartManagerTest.php
+++ tests/Service/CartManagerTest.php
@@ -2,8 +2,12 @@
 
 namespace App\Tests\Service;
 
+use App\Entity\Cart;
 use App\Entity\CartItem;
 use App\Entity\Product;
+use App\Repository\CartItemRepository;
+use App\Repository\CartRepository;
+use App\Repository\ProductRepository;
 use App\Service\CartManager;
 use InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -48,6 +52,121 @@ class CartManagerTest extends KernelTestCase
         $cartManager = static::getContainer()->get(CartManager::class);
         $this->expectException(InvalidArgumentException::class);
         $this->assertEquals(6, $cartManager->getCartItemTotal(null));
+    }
+
+    public function testAddNewProductToCart()
+    {
+        $product = new Product();
+        $product->setUnitPrice(1);
+
+        $productRepository = $this->createMock(ProductRepository::class);
+        $productRepository->expects($this->any())
+            ->method('find')
+            ->willReturn($product);
+
+        $cart = new Cart();
+        $cartRepository = $this->createMock(CartRepository::class);
+        $cartRepository->expects($this->any())
+            ->method('find')
+            ->willReturn($cart);
+
+        $cartItemRepository = $this->createMock(CartItemRepository::class);
+        $cartItemRepository->expects($this->any())
+            ->method('findOneBy')
+            ->willReturn(null);
+
+        $cartManager = new CartManager($productRepository, $cartRepository, $cartItemRepository);
+
+        /** @var CartItem $cartItem */
+        $cartItem = $cartManager->addProductToCart(3, 1);
+
+        $this->assertEquals(1, $cartItem->getQuantity());
+    }
+
+    public function testAddSameProductToCart()
+    {
+        $product = new Product();
+        $product->setUnitPrice(1);
+
+        $productRepository = $this->createMock(ProductRepository::class);
+        $productRepository->expects($this->any())
+            ->method('find')
+            ->willReturn($product);
+
+        $cart = new Cart();
+        $item = new CartItem();
+        $item->setProduct($product);
+        $item->setCart($cart);
+        $item->setQuantity(3);
+        $cart->addItem($item);
+
+        $cartItemRepository = $this->createMock(CartItemRepository::class);
+        $cartItemRepository->expects($this->any())
+            ->method('findOneBy')
+            ->willReturn($item);
+
+        $cartRepository = $this->createMock(CartRepository::class);
+        $cartRepository->expects($this->any())
+            ->method('find')
+            ->willReturn($cart);
+
+        $cartManager = new CartManager($productRepository, $cartRepository, $cartItemRepository);
+
+        /** @var CartItem $cartItem */
+        $cartItem = $cartManager->addProductToCart(3, 1);
+
+        $this->assertEquals(4, $cartItem->getQuantity());
+    }
+
+    public function testAddNullProductToCart()
+    {
+        $productRepository = $this->createMock(ProductRepository::class);
+        $productRepository->expects($this->any())
+            ->method('find')
+            ->willReturn(null);
+
+        $cart = new Cart();
+        $cartRepository = $this->createMock(CartRepository::class);
+        $cartRepository->expects($this->any())
+            ->method('find')
+            ->willReturn($cart);
+
+        $cartItemRepository = $this->createMock(CartItemRepository::class);
+        $cartItemRepository->expects($this->any())
+            ->method('findOneBy')
+            ->willReturn(null);
+
+        $cartManager = new CartManager($productRepository, $cartRepository, $cartItemRepository);
+
+        $this->expectException(InvalidArgumentException::class);
+        $cartManager->addProductToCart(42, 1);
+    }
+
+    public function testAddProductToNullCart()
+    {
+        $product = new Product();
+        $product->setUnitPrice(1);
+
+        $productRepository = $this->createMock(ProductRepository::class);
+        $productRepository->expects($this->any())
+            ->method('find')
+            ->willReturn($product);
+
+        $cart = new Cart();
+        $cartRepository = $this->createMock(CartRepository::class);
+        $cartRepository->expects($this->any())
+            ->method('find')
+            ->willReturn(null);
+
+        $cartItemRepository = $this->createMock(CartItemRepository::class);
+        $cartItemRepository->expects($this->any())
+            ->method('findOneBy')
+            ->willReturn(null);
+
+        $cartManager = new CartManager($productRepository, $cartRepository, $cartItemRepository);
+
+        $this->expectException(InvalidArgumentException::class);
+        $cartManager->addProductToCart(42, 1);
     }
 
     private function createCartItem(int $quantity, float $unitPrice): CartItem
